ac4e73afe87ee546bbe9743c6ddf9d659a623df4
Angelo <angelo.gonzalez@live.com>
2022-07-14 23:06:13 -0400

    86. Mostrar los resultados en pantalla

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,6 +9,9 @@ export class GifsService {
   private apiKey: string = 'MiXCXOn7o44bw9QbN6JdbrpBZ2uvrLYf';
   private _historial: string[] = [];
 
+  // TODO: cambiar por su tipo correspondiente
+  public resultados: any[]= [];
+
   get historial() {    
     return [...this._historial];
   }
@@ -24,9 +27,10 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }
 
-    this.http.get( 'https://api.giphy.com/v1/gifs/search?api_key=MiXCXOn7o44bw9QbN6JdbrpBZ2uvrLYf&q=dragon ball z&limit=10' )
+    this.http.get( `https://api.giphy.com/v1/gifs/search?api_key=MiXCXOn7o44bw9QbN6JdbrpBZ2uvrLYf&q=${ query }&limit=10` )
       .subscribe( ( resp: any) => {
         console.log(resp.data);
+        this.resultados = resp.data;
       })
 
     
