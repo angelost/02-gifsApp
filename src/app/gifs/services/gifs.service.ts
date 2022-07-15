7af9e2eb94f5e112bd76f862c27d4f7bc8b9cb28
Angelo <angelo.gonzalez@live.com>
2022-07-15 00:17:47 -0400

    89. Cargar imágenes automáticamente

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -20,6 +20,7 @@ export class GifsService {
   constructor( private http: HttpClient ) {
 
     this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
     // if ( localStorage.getItem('historial')) {
     //   this._historial = JSON.parse( localStorage.getItem('historial')!);
     // }
@@ -41,6 +42,7 @@ export class GifsService {
       .subscribe( ( resp: any) => {
         console.log(resp.data);
         this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
       })
 
     
