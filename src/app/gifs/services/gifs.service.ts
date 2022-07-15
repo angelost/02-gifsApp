a781f1166de478428c287ed0b036aaef4d8634c2
Angelo <angelo.gonzalez@live.com>
2022-07-15 00:05:48 -0400

    88. LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { JsonPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
@@ -16,7 +17,14 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor( private http: HttpClient ) {}
+  constructor( private http: HttpClient ) {
+
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    // if ( localStorage.getItem('historial')) {
+    //   this._historial = JSON.parse( localStorage.getItem('historial')!);
+    // }
+
+  }
 
   buscarGifs( query: string ) {
 
@@ -25,6 +33,8 @@ export class GifsService {
     if ( !this._historial.includes( query ) ) {
       this._historial.unshift( query );
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
     this.http.get<SearchGifsResponse>( `https://api.giphy.com/v1/gifs/search?api_key=MiXCXOn7o44bw9QbN6JdbrpBZ2uvrLYf&q=${ query }&limit=10` )
